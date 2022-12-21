df07acf0eadc4dd006b6627e6053123c5888497c
Christophe Fondeville <fondevillechristophe@gmail.com>
2022-12-21 20:18:48 +0100

    Initial commit

---
+++ src/main/java/com/exemple/crudjavaapi/CrudJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.exemple.crudjavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJavaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudJavaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exemple/crudjavaapi/CrudJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.crudjavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJavaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
